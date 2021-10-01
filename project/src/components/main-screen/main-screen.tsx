fb54601518864773b9d7d54d6f404b280b566793
Dmitrii <dp086@mail.ru>
2021-10-01 20:30:31 +0300

    Исправил ошибку

--- project/src/components/main-screen/main-screen.tsx
+++ project/src/components/main-screen/main-screen.tsx
@@ -11,23 +11,23 @@ function MainScreen({placeCardsCount}: MainScreenProps): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <button className="header__logo-link header__logo-link--active">
+              <a className="header__logo-link header__logo-link--active" href="main.html">
                 <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </button>
+              </a>
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
-                  <button className="header__nav-link header__nav-link--profile">
+                  <a className="header__nav-link header__nav-link--profile" href="main.html">
                     <div className="header__avatar-wrapper user__avatar-wrapper">
                     </div>
                     <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </button>
+                  </a>
                 </li>
                 <li className="header__nav-item">
-                  <button className="header__nav-link">
+                  <a className="header__nav-link" href="main.html">
                     <span className="header__signout">Sign out</span>
-                  </button>
+                  </a>
                 </li>
               </ul>
             </nav>
@@ -41,34 +41,34 @@ function MainScreen({placeCardsCount}: MainScreenProps): JSX.Element {
           <section className="locations container">
             <ul className="locations__list tabs__list">
               <li className="locations__item">
-                <button className="locations__item-link tabs__item">
+                <a className="locations__item-link tabs__item" href="main.html">
                   <span>Paris</span>
-                </button>
+                </a>
               </li>
               <li className="locations__item">
-                <button className="locations__item-link tabs__item">
+                <a className="locations__item-link tabs__item" href="main.html">
                   <span>Cologne</span>
-                </button>
+                </a>
               </li>
               <li className="locations__item">
-                <button className="locations__item-link tabs__item">
+                <a className="locations__item-link tabs__item" href="main.html">
                   <span>Brussels</span>
-                </button>
+                </a>
               </li>
               <li className="locations__item">
-                <button className="locations__item-link tabs__item tabs__item--active">
+                <a className="locations__item-link tabs__item tabs__item--active" href="main.html">
                   <span>Amsterdam</span>
-                </button>
+                </a>
               </li>
               <li className="locations__item">
-                <button className="locations__item-link tabs__item">
+                <a className="locations__item-link tabs__item" href="main.html">
                   <span>Hamburg</span>
-                </button>
+                </a>
               </li>
               <li className="locations__item">
-                <button className="locations__item-link tabs__item">
+                <a className="locations__item-link tabs__item" href="main.html">
                   <span>Dusseldorf</span>
-                </button>
+                </a>
               </li>
             </ul>
           </section>
@@ -77,7 +77,7 @@ function MainScreen({placeCardsCount}: MainScreenProps): JSX.Element {
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{placeCardsCount} places to stay in Amsterdam</b>
+              <b className="places__found">312 places to stay in Amsterdam</b>
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
                 <span className="places__sorting-type" tabIndex={0}>
@@ -86,6 +86,12 @@ function MainScreen({placeCardsCount}: MainScreenProps): JSX.Element {
                     <use xlinkHref="#icon-arrow-select"></use>
                   </svg>
                 </span>
+                <ul className="places__options places__options--custom places__options--opened">
+                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
+                  <li className="places__option" tabIndex={0}>Price: low to high</li>
+                  <li className="places__option" tabIndex={0}>Price: high to low</li>
+                  <li className="places__option" tabIndex={0}>Top rated first</li>
+                </ul>
               </form>
               <div className="cities__places-list places__list tabs__content">
                 {new Array(placeCardsCount).fill(PlaceCard).map((placeCard, i) => placeCard(i))}
