08d7513163990debf1c255e760160345280ddc27
Dmitrii <dp086@mail.ru>
2021-10-01 19:39:24 +0300

    Добавил компоненты для отрисовки главной страницы и карточки места

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  PLACE_CARDS_COUNT: 7,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App placeCardsCount={Setting.PLACE_CARDS_COUNT}/>
   </React.StrictMode>,
   document.getElementById('root'));
