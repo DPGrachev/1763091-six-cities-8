25a97c4202dce38af0b6bed962192fc48a6353cd
Dmitrii <dp086@mail.ru>
2021-10-23 21:59:42 +0300

    сделал логику сортировки карточек по выбранному варианту сортировки

--- project/src/components/main-screen/main-screen.tsx
+++ project/src/components/main-screen/main-screen.tsx
@@ -7,6 +7,7 @@ import CitiesList from '../cities-list/cities-list';
 import {connect, ConnectedProps} from 'react-redux';
 import {State} from '../../types/state';
 import { Offer } from '../../types/offer';
+import { SortOption } from '../../const';
 
 const getCitiesCoordinates = (city:string) => {
   switch(city){
@@ -26,8 +27,26 @@ type MainScreenProps = {
   offers: Offer[],
 }
 
-const mapStateToProps = ({currentCity}:State) => ({
+const getSortedOffers = (currentSortOption : string, offers: Offer[]) => {
+  switch(currentSortOption){
+    case SortOption.PriceHighToLow: {
+      return offers.slice().sort((offerA, offerB) => offerB.price - offerA.price);
+    }
+    case SortOption.PriceLowToHigh: {
+      return offers.slice().sort((offerA, offerB) => offerA.price - offerB.price);
+    }
+    case SortOption.TopRatedFirst: {
+      return offers.slice().sort((offerA, offerB) => offerB.rating - offerA.rating);
+    }
+    default: {
+      return offers;
+    }
+  }
+};
+
+const mapStateToProps = ({currentCity, currentSortOption}:State) => ({
   currentCity,
+  currentSortOption,
 });
 
 const connector = connect(mapStateToProps);
@@ -35,8 +54,9 @@ const connector = connect(mapStateToProps);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 type ConnectedComponentProps = PropsFromRedux & MainScreenProps;
 
-function MainScreen({offers, currentCity}: ConnectedComponentProps): JSX.Element {
+function MainScreen({offers, currentCity, currentSortOption}: ConnectedComponentProps): JSX.Element {
   const [idActiveOffer, setIdActiveOffer] = useState<null | number>(null);
+  const sortedOffers = getSortedOffers(currentSortOption, offers);
 
   return (
     <div className="page page--gray page--main">
@@ -77,7 +97,7 @@ function MainScreen({offers, currentCity}: ConnectedComponentProps): JSX.Element
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{offers.length} places to stay in {currentCity}</b>
               <Sort />
-              <OffersList offers={offers} onCardFocus={setIdActiveOffer}/>
+              <OffersList offers={sortedOffers} onCardFocus={setIdActiveOffer}/>
             </section>
             <div className="cities__right-section">
               <Map city={getCitiesCoordinates(currentCity)} offers={offers} idActiveOffer={idActiveOffer}/>
