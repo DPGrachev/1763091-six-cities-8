e5c8b71a76f229c4a1cf7463e5abae6c7835d938
Dmitrii <dp086@mail.ru>
2021-11-02 16:51:12 +0300

    добавил компонент для отрисовки пустого экрана с избранными объявлениями

--- project/src/components/favorites-screen/favorites-screen.tsx
+++ project/src/components/favorites-screen/favorites-screen.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
-import Logo from '../header/header';
+import Header from '../header/header';
+import EmptyFavoriteScreen from '../empty-favorite-screen/empty-favorite-screen';
 import {AppRoute} from '../../const';
 import FavoriteOfferCard from '../favorite-offer-card/favorite-offer-card';
 import {useDispatch, useSelector} from 'react-redux';
@@ -17,53 +18,32 @@ function FavoritesScreen(): JSX.Element{
 
   return (
     <div className="page">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Logo />
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="/">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
+      <Header />
+      {favoriteOffers.length === 0
+        ? <EmptyFavoriteScreen />
+        :
+        <main className="page__main page__main--favorites">
+          <div className="page__favorites-container container">
+            <section className="favorites">
+              <h1 className="favorites__title">Saved listing</h1>
+              <ul className="favorites__list">
+                <li className="favorites__locations-items">
+                  <div className="favorites__locations locations locations--current">
+                    <div className="locations__item">
+                      <a className="locations__item-link" href="/">
+                        <span>Amsterdam</span>
+                      </a>
                     </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="/">
-                    <span className="header__signout">Sign out</span>
-                  </a>
+                  </div>
+                  <div className="favorites__places">
+                    {favoriteOffers.map((favoriteOfferCard) => <FavoriteOfferCard key={favoriteOfferCard.id} offer={favoriteOfferCard}/>)}
+                  </div>
                 </li>
               </ul>
-            </nav>
+            </section>
           </div>
-        </div>
-      </header>
+        </main>}
 
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-              <li className="favorites__locations-items">
-                <div className="favorites__locations locations locations--current">
-                  <div className="locations__item">
-                    <a className="locations__item-link" href="/">
-                      <span>Amsterdam</span>
-                    </a>
-                  </div>
-                </div>
-                <div className="favorites__places">
-                  {favoriteOffers.map((favoriteOfferCard) => <FavoriteOfferCard key={favoriteOfferCard.id} offer={favoriteOfferCard}/>)}
-                </div>
-              </li>
-            </ul>
-          </section>
-        </div>
-      </main>
       <footer className="footer container">
         <Link className="footer__logo-link" to={AppRoute.Main}>
           <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
