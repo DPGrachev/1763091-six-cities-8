3e63b8d2de8f390688f90c861305271580dba01f
Dmitrii <dp086@mail.ru>
2021-11-09 15:51:55 +0300

    добавил тест для карты

---
+++ project/src/components/map/map.test.tsx
@@ -0,0 +1,21 @@
+import {render} from '@testing-library/react';
+import Map from './map';
+import { CityName } from '../../const';
+import { offers } from '../../utils/mocks';
+
+describe('Component: Map', () => {
+  const currentCity = CityName.Amsterdam;
+
+  it('should render correctly in main screen', () => {
+    const {container} = render(<Map city={currentCity} offers={offers}/>);
+
+    expect(container.querySelector('.cities__map')).toBeInTheDocument();
+    expect(container.querySelector('.property__map')).not.toBeInTheDocument();
+  });
+
+  it('should render correctly in room screen', () => {
+    const {container} = render(<Map city={currentCity} offers={offers} isRoomScreenMap/>);
+
+    expect(container.querySelector('.cities__map')).not.toBeInTheDocument();
+    expect(container.querySelector('.property__map')).toBeInTheDocument();  });
+});
