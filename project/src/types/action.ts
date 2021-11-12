a54b402511bbc8ade4189f27e4ddf14fc70cb610
Dmitrii <dp086@mail.ru>
2021-11-12 15:23:58 +0300

    удалил неиспользуемый код

--- project/src/types/action.ts
+++ project/src/types/action.ts
@@ -1,10 +1,8 @@
-import { ThunkAction, ThunkDispatch } from 'redux-thunk';
+import { ThunkAction } from 'redux-thunk';
 import { AxiosInstance } from 'axios';
 import { State } from '../types/state';
 import {Action} from 'redux';
 
 type ThunkActionResult<R = Promise<void>> = ThunkAction<R, State, AxiosInstance, Action>;
 
-type ThunkAppDispatch = ThunkDispatch<State, AxiosInstance, Action>;
-
-export type {ThunkActionResult, ThunkAppDispatch};
+export type {ThunkActionResult};
